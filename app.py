2fae6c3feb5daea5b6a0109d7945ba80d8719d5b
Brentaboy <@brent.aniagyei@gmail.com>
2021-07-29 14:16:07 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+
+app = Flask(__name__)
+
+@app.route("/")
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+
+
+
+@app.route('/products')
+def products():
+    return render_template("products.html")
+
+
+@app.route('/total')
+def total():
+
+    return render_template("total.html")
