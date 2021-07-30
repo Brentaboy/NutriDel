cdfd854d582ffe202949b8d5f15d771c2c7c6aeb
Brentaboy <@brent.aniagyei@gmail.com>
2021-07-30 14:21:08 +0000

    python update 2

--- app.py
+++ app.py
@@ -5,21 +5,19 @@ from flask import request
 
 app = Flask(__name__)
 
-@app.route("/")
 
+@app.route("/")
 @app.route('/home')
 def home():
     return render_template("home.html")
 
 
-
-
 @app.route('/products')
 def products():
+
     return render_template("products.html")
 
 
 @app.route('/total')
 def total():
-
     return render_template("total.html")
